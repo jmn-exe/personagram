53c3602d644c76e43f90104d23e8e5987dd2bfa5
Arzmin Grunsin <72203908+jmn-exe@users.noreply.github.com>
2023-10-09 18:45:56 +0800

    post view and grid

--- client/src/App.js
+++ client/src/App.js
@@ -22,16 +22,44 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __importStar(require("react"));
 var react_1 = require("react");
+require("./App.css");
+var data_json_1 = __importDefault(require("./data/data.json"));
+function Post(props) {
+    return (React.createElement("div", { className: 'img-instance', key: props.postID, onClick: function () { return OpenPost(props.postID); } },
+        React.createElement("img", { src: props.url, alt: props.alt })));
+}
+function OpenPost(id) {
+    data_json_1.default.map(function (data) {
+        if (data.id == id) {
+        }
+    });
+}
 function App() {
+    console.log(typeof (data_json_1.default));
     var _a = (0, react_1.useState)([{}]), backendData = _a[0], setBackendData = _a[1];
-    (0, react_1.useEffect)(function () {
-        fetch("/api").then(function (response) { return response.json(); }).then(function (data) {
-            setBackendData(data);
-        });
-    }, []);
-    return (React.createElement("div", null, (typeof backendData.users === 'undefined') ? (React.createElement("p", null, "Loading....")) : (backendData.users.map(function (user, i) { return (React.createElement("p", { key: i }, user)); }))));
+    return (React.createElement("div", { className: 'main-container' },
+        React.createElement("h1", null, "PersonaGram"),
+        React.createElement("div", { className: "grid-center" }),
+        React.createElement("div", { className: 'post-view' },
+            React.createElement("div", { className: 'dim-back' },
+                React.createElement("div", { className: 'post-box' },
+                    React.createElement("div", { className: 'img-view' },
+                        React.createElement("img", { src: "https://i.ytimg.com/vi/tzzgVtEAoqw/maxresdefault.jpg", alt: "" })),
+                    React.createElement("div", { className: 'desc-view' },
+                        React.createElement("div", { className: 'post-date' }, "Last edited - 6:11PM 9/10/2023"),
+                        React.createElement("div", { className: 'post-tags' },
+                            React.createElement("div", null, "Diary"),
+                            React.createElement("div", null, "Journal"),
+                            React.createElement("div", null, "Fun")),
+                        React.createElement("div", { className: 'post-desc' }, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut hendrerit felis. Cras eleifend consequat nunc nec placerat. Maecenas lobortis mauris sit amet est aliquet porta. Nulla eros risus, dictum sed nibh in, viverra vehicula nisl. Proin finibus metus sit amet nisl dapibus malesuada. Nulla facilisi. Suspendisse sed nisl et leo molestie posuere eu sit amet erat."),
+                        React.createElement("div", { className: 'post-edit-btn' }, "Edit")))))));
+}
+function fetchData() {
 }
 exports.default = App;
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,65 @@
 import * as React from 'react'
 import {useEffect, useState} from 'react'
+import './App.css'
+import postData from './data/data.json'
+
+function Post(props){
+  return(
+    <div className='img-instance' key={props.postID} onClick={()=>OpenPost(props.postID)}>
+      <img src={props.url} alt={props.alt}></img>
+    </div>
+  )
+}
+
+function OpenPost(id){
+  postData.map((data)=>{
+    if(data.id == id){
+
+    }
+  })
+}
 
 function App() {
-
-  const [backendData, setBackendData] : any[] = useState([{}]) 
-
-  useEffect(()=>{
-    fetch("/api").then(
-      response => response.json()
-  ).then(
-    data => {
-      setBackendData(data)
-    }
-  )
-  },[]);
+  console.log(typeof(postData));
+  const [backendData, setBackendData] : any[] = useState([{}]);
   return(
-    <div>
-    {(typeof backendData.users === 'undefined') ? (
-      <p>Loading....</p>
-    ):(
-      backendData.users.map((user,i)=>(
-        <p key={i}>{user}</p>
-      ))
-    )}
+    <div className='main-container'>
+    <h1>PersonaGram</h1>
+    <div className="grid-center">
+      {/* 
+      <div className="grid-container">
+        {postData.map((data)=> <Post postID={data.id} url={data.image.url}
+        alt={data.image.alt}/>)}
+      </div>
+      */}
+    </div>
+    <div className='post-view'>
+        <div className='dim-back'>
+          <div className='post-box'>
+            <div className='img-view'>
+              <img src="https://i.ytimg.com/vi/tzzgVtEAoqw/maxresdefault.jpg" alt="" />
+            </div>
+            <div className='desc-view'>
+              <div className='post-date'>Last edited - 6:11PM 9/10/2023</div>
+              <div className='post-tags'>
+                <div>Diary</div>
+                <div>Journal</div>
+                <div>Fun</div>
+              </div>
+              <div className='post-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut hendrerit felis. Cras eleifend consequat nunc nec placerat. Maecenas lobortis mauris sit amet est aliquet porta. Nulla eros risus, dictum sed nibh in, viverra vehicula nisl. Proin finibus metus sit amet nisl dapibus malesuada. Nulla facilisi. Suspendisse sed nisl et leo molestie posuere eu sit amet erat.</div>
+              <div className='post-edit-btn'>Edit</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
     </div>
   );
+}
+
+
+function fetchData(){
+
 }
 
 export default App;
