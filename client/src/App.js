72e059d69ea653209627b11d07931491baf36383
Arzmin Grunsin <72203908+jmn-exe@users.noreply.github.com>
2023-10-09 23:25:05 +0800

    added back button

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,76 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var React = __importStar(require("react"));
-var react_1 = require("react");
+var react_1 = __importDefault(require("react"));
+var react_2 = require("react");
 require("./App.css");
 var data_json_1 = __importDefault(require("./data/data.json"));
 function Post(props) {
-    return (React.createElement("div", { className: 'img-instance', key: props.postID, onClick: function () { return OpenPost(props.postID); } },
-        React.createElement("img", { src: props.url, alt: props.alt })));
+    return (react_1.default.createElement("div", { className: 'img-instance', key: props.postID, onClick: function () { return OpenPost(props); } },
+        react_1.default.createElement("img", { src: props.url, alt: props.alt })));
 }
-function OpenPost(id) {
-    data_json_1.default.map(function (data) {
-        if (data.id == id) {
-        }
-    });
+function OpenPost(props) {
+    return (react_1.default.createElement("div", { className: 'post-view' },
+        react_1.default.createElement("div", { className: 'dim-back' },
+            react_1.default.createElement("div", { className: 'post-box' },
+                react_1.default.createElement("div", { className: "post-back-btn" }, "\u2190 Back"),
+                react_1.default.createElement("div", { className: 'img-view' },
+                    react_1.default.createElement("img", { src: "https://i.ytimg.com/vi/tzzgVtEAoqw/maxresdefault.jpg", alt: "" })),
+                react_1.default.createElement("div", { className: 'desc-view' },
+                    react_1.default.createElement("div", { className: 'post-date' }, "Last edited - 6:11PM 9/10/2023"),
+                    react_1.default.createElement("div", { className: 'post-tags' },
+                        react_1.default.createElement("div", null, "Diary"),
+                        react_1.default.createElement("div", null, "Journal"),
+                        react_1.default.createElement("div", null, "Fun")),
+                    react_1.default.createElement("div", { className: 'post-desc' }, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut hendrerit felis. Cras eleifend consequat nunc nec placerat. Maecenas lobortis mauris sit amet est aliquet porta. Nulla eros risus, dictum sed nibh in, viverra vehicula nisl. Proin finibus metus sit amet nisl dapibus malesuada. Nulla facilisi. Suspendisse sed nisl et leo molestie posuere eu sit amet erat."),
+                    react_1.default.createElement("div", { className: 'post-edit-btn' }, "Edit"))))));
 }
-function App() {
-    console.log(typeof (data_json_1.default));
-    var _a = (0, react_1.useState)([{}]), backendData = _a[0], setBackendData = _a[1];
-    return (React.createElement("div", { className: 'main-container' },
-        React.createElement("h1", null, "PersonaGram"),
-        React.createElement("div", { className: "grid-center" }),
-        React.createElement("div", { className: 'post-view' },
-            React.createElement("div", { className: 'dim-back' },
-                React.createElement("div", { className: 'post-box' },
-                    React.createElement("div", { className: 'img-view' },
-                        React.createElement("img", { src: "https://i.ytimg.com/vi/tzzgVtEAoqw/maxresdefault.jpg", alt: "" })),
-                    React.createElement("div", { className: 'desc-view' },
-                        React.createElement("div", { className: 'post-date' }, "Last edited - 6:11PM 9/10/2023"),
-                        React.createElement("div", { className: 'post-tags' },
-                            React.createElement("div", null, "Diary"),
-                            React.createElement("div", null, "Journal"),
-                            React.createElement("div", null, "Fun")),
-                        React.createElement("div", { className: 'post-desc' }, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut hendrerit felis. Cras eleifend consequat nunc nec placerat. Maecenas lobortis mauris sit amet est aliquet porta. Nulla eros risus, dictum sed nibh in, viverra vehicula nisl. Proin finibus metus sit amet nisl dapibus malesuada. Nulla facilisi. Suspendisse sed nisl et leo molestie posuere eu sit amet erat."),
-                        React.createElement("div", { className: 'post-edit-btn' }, "Edit")))))));
+function ShowMain() {
+    return (react_1.default.createElement("div", { className: "grid-center" },
+        react_1.default.createElement("div", { className: "grid-container" }, data_json_1.default.map(function (data) { return react_1.default.createElement(Post, { postID: data.id, url: data.image.url, alt: data.image.alt }); }))));
 }
-function fetchData() {
-}
+var App = /** @class */ (function (_super) {
+    __extends(App, _super);
+    function App() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    //const [backendData, setBackendData] : any[] = useState([{}]);
+    App.prototype.render = function () {
+        return (react_1.default.createElement("div", { className: 'main-container' },
+            react_1.default.createElement("h1", null, "PersonaGram"),
+            react_1.default.createElement("div", { className: "grid-center" }),
+            react_1.default.createElement("div", { className: 'post-view' },
+                react_1.default.createElement("div", { className: 'dim-back' },
+                    react_1.default.createElement("div", { className: 'post-box' },
+                        react_1.default.createElement("div", { className: "post-back-btn", onClick: function () { return ShowMain(); } }, "\u2190 Back"),
+                        react_1.default.createElement("div", { className: 'img-view' },
+                            react_1.default.createElement("img", { src: "https://i.ytimg.com/vi/tzzgVtEAoqw/maxresdefault.jpg", alt: "" })),
+                        react_1.default.createElement("div", { className: 'desc-view' },
+                            react_1.default.createElement("div", { className: 'post-date' }, "Last edited - 6:11PM 9/10/2023"),
+                            react_1.default.createElement("div", { className: 'post-tags' },
+                                react_1.default.createElement("div", null, "Diary"),
+                                react_1.default.createElement("div", null, "Journal"),
+                                react_1.default.createElement("div", null, "Fun")),
+                            react_1.default.createElement("div", { className: 'post-desc' }, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut hendrerit felis. Cras eleifend consequat nunc nec placerat. Maecenas lobortis mauris sit amet est aliquet porta. Nulla eros risus, dictum sed nibh in, viverra vehicula nisl. Proin finibus metus sit amet nisl dapibus malesuada. Nulla facilisi. Suspendisse sed nisl et leo molestie posuere eu sit amet erat."),
+                            react_1.default.createElement("div", { className: 'post-edit-btn' }, "Edit")))))));
+    };
+    return App;
+}(react_2.Component));
 exports.default = App;
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,73 @@
-import * as React from 'react'
+import React from 'react'
+import {Component} from 'react'
 import {useEffect, useState} from 'react'
 import './App.css'
 import postData from './data/data.json'
 
 function Post(props){
   return(
-    <div className='img-instance' key={props.postID} onClick={()=>OpenPost(props.postID)}>
+    <div className='img-instance' key={props.postID} onClick={()=>OpenPost(props)}>
       <img src={props.url} alt={props.alt}></img>
     </div>
   )
 }
 
-function OpenPost(id){
-  postData.map((data)=>{
-    if(data.id == id){
-
-    }
-  })
+function OpenPost(props){
+  return(
+    <div className='post-view'>
+      <div className='dim-back'>
+        <div className='post-box'>
+          <div className="post-back-btn">← Back</div>
+          <div className='img-view'>
+            <img src="https://i.ytimg.com/vi/tzzgVtEAoqw/maxresdefault.jpg" alt="" />
+          </div>
+          <div className='desc-view'>
+            <div className='post-date'>Last edited - 6:11PM 9/10/2023</div>
+            <div className='post-tags'>
+              <div>Diary</div>
+              <div>Journal</div>
+              <div>Fun</div>
+            </div>
+            <div className='post-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut hendrerit felis. Cras eleifend consequat nunc nec placerat. Maecenas lobortis mauris sit amet est aliquet porta. Nulla eros risus, dictum sed nibh in, viverra vehicula nisl. Proin finibus metus sit amet nisl dapibus malesuada. Nulla facilisi. Suspendisse sed nisl et leo molestie posuere eu sit amet erat.</div>
+            <div className='post-edit-btn'>Edit</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-function App() {
-  console.log(typeof(postData));
-  const [backendData, setBackendData] : any[] = useState([{}]);
+function ShowMain(){
   return(
-    <div className='main-container'>
-    <h1>PersonaGram</h1>
-    <div className="grid-center">
-      {/* 
+    <div className="grid-center"> 
       <div className="grid-container">
         {postData.map((data)=> <Post postID={data.id} url={data.image.url}
         alt={data.image.alt}/>)}
       </div>
-      */}
     </div>
-    <div className='post-view'>
+  );
+}
+
+export default class App extends Component{
+
+
+  //const [backendData, setBackendData] : any[] = useState([{}]);
+  render(){
+    return(
+      <div className='main-container'>
+      <h1>PersonaGram</h1>
+      <div className="grid-center">
+        {/* 
+        <div className="grid-container">
+          {postData.map((data)=> <Post postID={data.id} url={data.image.url}
+          alt={data.image.alt}/>)}
+        </div>
+        */}
+      </div>
+      <div className='post-view'>
         <div className='dim-back'>
           <div className='post-box'>
+            <div className="post-back-btn" onClick={()=>ShowMain()}>← Back</div>
             <div className='img-view'>
               <img src="https://i.ytimg.com/vi/tzzgVtEAoqw/maxresdefault.jpg" alt="" />
             </div>
@@ -52,14 +84,9 @@ function App() {
           </div>
         </div>
       </div>
-    
-    </div>
-  );
+      
+      </div>
+    );
+  }
 }
 
-
-function fetchData(){
-
-}
-
-export default App;
