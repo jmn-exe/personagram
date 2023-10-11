35257c205a9e2ac62f79df7dc96aee82f973bed6
Arzmin Grunsin <72203908+jmn-exe@users.noreply.github.com>
2023-10-11 12:49:31 +0800

    edit post added yippieee

--- client/src/pages/PostView.js
+++ client/src/pages/PostView.js
@@ -1,9 +1,19 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importDefault(require("react"));
+var react_2 = require("react");
 var react_router_dom_1 = require("react-router-dom");
 var react_router_dom_2 = require("react-router-dom");
 require("../App.css");
@@ -13,12 +23,17 @@ var postTags = [
     { tag: 'Journal', class: 't-journal' },
     { tag: 'Fun', class: 't-fun' },
 ];
-//console.log(JSON.stringify(postData));
+console.log(JSON.stringify(data_json_1.default));
 //let {id} = useParams();
 //console.log(id);
 //console.log(typeof(id));
 function ShowPost(props) {
     var post = data_json_1.default.find(function (p) { return p.id === props.id; });
+    var initNote = post.note;
+    var initTag = post.tag;
+    var _a = (0, react_2.useState)(initNote), sPostNote = _a[0], setPostNote = _a[1];
+    var _b = (0, react_2.useState)(initTag), sPostTag = _b[0], setPostTag = _b[1];
+    var _c = (0, react_2.useState)(0), isEdit = _c[0], setIsEdit = _c[1];
     if (post != null) {
         return (react_1.default.createElement("div", { className: 'post-view' },
             react_1.default.createElement("div", { className: 'post-container' },
@@ -27,19 +42,37 @@ function ShowPost(props) {
                         react_1.default.createElement("div", { className: "post-back-btn" }, "\u2190 Back")),
                     react_1.default.createElement("div", { className: 'img-view' },
                         react_1.default.createElement("img", { src: post.image.url, alt: post.image.alt })),
-                    react_1.default.createElement("div", { className: 'desc-view pv' },
+                    react_1.default.createElement("div", { className: 'desc-view ' + (isEdit ? 'pv' : '') },
                         react_1.default.createElement("div", { className: 'post-date' },
                             "Last edited - ",
                             getDate(post.datemodified)),
                         react_1.default.createElement("div", { className: 'post-tags' }, post.tag.map(function (t) { return (react_1.default.createElement(Tag, { tag: t })); })),
                         react_1.default.createElement("div", { className: 'post-desc' }, post.note),
-                        react_1.default.createElement("div", { className: 'post-edit-btn' }, "Edit")),
-                    react_1.default.createElement("div", { className: 'desc-edit' },
+                        react_1.default.createElement("button", { className: 'post-edit-btn', onClick: function () { return setIsEdit(1); } }, "Edit")),
+                    react_1.default.createElement("div", { className: 'desc-edit ' + (isEdit ? '' : 'pv') },
                         react_1.default.createElement("div", { className: 'tags-select' },
-                            react_1.default.createElement(TagSelector, { tags: post.tag })),
-                        react_1.default.createElement("div", { className: 'post-desc' }, post.note),
-                        react_1.default.createElement("div", { className: 'post-edit-btn' }, "Edit"))))));
+                            react_1.default.createElement(TagSelector, { tags: sPostTag, func: setPostTag })),
+                        react_1.default.createElement("textarea", { className: 'post-desc', value: sPostNote, onChange: function (t) { return setPostNote(t.target.value); } }),
+                        react_1.default.createElement("div", { className: 'edit-confirm' },
+                            react_1.default.createElement("button", { className: 'save-edit', onClick: function () {
+                                    savePost(post.id, sPostNote, sPostTag);
+                                    setIsEdit(0);
+                                } }, "Save"),
+                            react_1.default.createElement("button", { className: 'cancel-edit', onClick: function () {
+                                    var confirmation = window.confirm("Discard changes?");
+                                    if (confirmation) {
+                                        setPostNote(initNote);
+                                        setPostTag(initTag);
+                                        setIsEdit(0);
+                                    }
+                                } }, "Cancel")))))));
     }
+}
+function savePost(id, note, tags) {
+    var postIndex = data_json_1.default.findIndex(function (p) { return p.id === id; });
+    data_json_1.default[postIndex].note = note;
+    data_json_1.default[postIndex].tag = tags;
+    data_json_1.default[postIndex].datemodified = JSON.stringify(Date.now());
 }
 function getDate(date) {
     if (typeof (date) == "string") {
@@ -58,11 +91,19 @@ function Tag(props) {
 }
 function TagSelector(props) {
     var tagList = postTags.map(function (t) {
-        return (react_1.default.createElement("label", { className: t.class + " .tag-select" },
+        return (react_1.default.createElement("label", { key: t.tag, className: t.class + " .tag-select" },
             t.tag,
-            react_1.default.createElement("input", { type: 'checkbox', id: t.tag, name: t.tag, value: t.tag, defaultChecked: props.tags.includes(t.tag) ? true : false })));
+            react_1.default.createElement("input", { key: t.tag, type: 'checkbox', id: t.tag, name: t.tag, value: t.tag, onChange: function (e) { return handleCheckbox(e, props.tags, props.func); }, defaultChecked: props.tags.includes(t.tag) ? true : false })));
     });
     return (react_1.default.createElement(react_1.default.Fragment, null, tagList));
+}
+function handleCheckbox(e, tags, setTags) {
+    if (e.target.checked) {
+        setTags(__spreadArray(__spreadArray([], tags, true), [e.target.value], false));
+    }
+    else {
+        setTags(tags.filter(function (item) { return item !== e.target.value; }));
+    }
 }
 function PostView() {
     var id = (0, react_router_dom_2.useParams)().id;
--- client/src/pages/PostView.tsx
+++ client/src/pages/PostView.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
 import '../App.css'
@@ -8,14 +9,19 @@ const postTags = [
     {tag:'Diary',class:'t-diary'},
     {tag:'Journal',class:'t-journal'},
     {tag:'Fun',class:'t-fun'},
-]
+];
 
-//console.log(JSON.stringify(postData));
+console.log(JSON.stringify(postData));
 //let {id} = useParams();
 //console.log(id);
 //console.log(typeof(id));
 function ShowPost(props){
     let post = postData.find(p => p.id === props.id);
+    let initNote = post.note;
+    let initTag = post.tag;
+    const [sPostNote,setPostNote] = useState(initNote);
+    const [sPostTag,setPostTag] = useState(initTag);
+    const [isEdit,setIsEdit] = useState(0);
     if(post != null){
         return(
         <div className='post-view'>
@@ -25,7 +31,7 @@ function ShowPost(props){
                     <div className='img-view'>
                         <img src={post.image.url} alt={post.image.alt} />
                     </div>
-                    <div className='desc-view pv'>
+                    <div className={'desc-view '+ (isEdit ? 'pv':'')}>
                         <div className='post-date'>Last edited - {getDate(post.datemodified)}</div>
                         <div className='post-tags'>
                             {post.tag.map(t => (
@@ -33,20 +39,51 @@ function ShowPost(props){
                             ))}
                         </div>
                         <div className='post-desc'>{post.note}</div>
-                        <div className='post-edit-btn'>Edit</div>
+                        <button
+                        className='post-edit-btn'
+                        onClick={()=>setIsEdit(1)}
+                        >Edit</button>
                     </div>
-                    <div className='desc-edit'>
+                    <div className={'desc-edit ' + (isEdit ? '':'pv')}>
                         <div className='tags-select'>
-                            <TagSelector tags={post.tag}/>
+                            <TagSelector tags={sPostTag} func={setPostTag}/>
                         </div>
-                        <div className='post-desc'>{post.note}</div>
-                        <div className='post-edit-btn'>Edit</div>
+                        <textarea className='post-desc' 
+                        value={sPostNote}
+                        onChange={(t)=> setPostNote(t.target.value)}
+                        ></textarea>
+                        <div className='edit-confirm'>
+                            <button 
+                            className='save-edit' 
+                            onClick={()=> {
+                                savePost(post.id,sPostNote,sPostTag);
+                                setIsEdit(0);
+                            }}>
+                            Save
+                            </button>
+                            <button className='cancel-edit' onClick={()=>{
+                                const confirmation = window.confirm("Discard changes?");
+                                if(confirmation){
+                                    setPostNote(initNote);
+                                    setPostTag(initTag);
+                                    setIsEdit(0);
+                                }
+                            }}>Cancel</button>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
         </div>
         )
     } 
+}
+
+function savePost(id,note,tags){
+    let postIndex = postData.findIndex(p => p.id === id)
+    postData[postIndex].note = note;
+    postData[postIndex].tag = tags;
+    postData[postIndex].datemodified = JSON.stringify(Date.now());
 }
 
 function getDate(date){
@@ -72,12 +109,14 @@ function Tag(props){
 function TagSelector(props){
     let tagList = postTags.map(t => {
         return(
-            <label className={t.class+" .tag-select"}>{t.tag}
+            <label key={t.tag} className={t.class+" .tag-select"}>{t.tag}
             <input 
+            key={t.tag}
             type='checkbox' 
             id={t.tag} 
             name={t.tag} 
             value={t.tag}
+            onChange={(e)=>handleCheckbox(e,props.tags,props.func)}
             defaultChecked={props.tags.includes(t.tag)? true: false}/>
             </label>
         );
@@ -87,6 +126,13 @@ function TagSelector(props){
         {tagList}
         </>
     );
+}
+function handleCheckbox(e,tags,setTags) {
+    if (e.target.checked) {
+        setTags([...tags, e.target.value]);
+    } else {
+        setTags(tags.filter((item) => item !== e.target.value));
+    }
 }
 
 export default function PostView(){
